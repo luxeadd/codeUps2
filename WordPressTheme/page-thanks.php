1ee840ccac6c4d87681668c6f30101c628a9b5b5
satoshi kouno <luxeadd@gmail.com>
2022-03-29 22:27:35 +0900

    03292227

--- WordPressTheme/page-thanks.php
+++ WordPressTheme/page-thanks.php
@@ -5,18 +5,9 @@
    <div class="p-lower-thanks__text">お問い合わせ完了</div><!-- /.p-lower-thanks__text -->
    <div class="p-lower-thanks__text">3営業日以内に<br class="sp-only">返信させて頂きます</div><!-- /.p-lower-thanks__text -->
   
-   <?php
-        $home = esc_url( home_url( "/" ));
-        $news = esc_url( home_url( "/news/" ));
-        $content = esc_url( home_url( "/content/" ));
-        $works = esc_url( home_url( "/works/" ));
-        $overview = esc_url( home_url( "/overview/" ));
-        $blog = esc_url( home_url( "/blog/" ));
-        $contact = esc_url( home_url( "/contact/" ));
-      ?>
-
+   <?php $home = esc_url( home_url( "/" ));  ?>
    <div class="p-lower-thanks__btn--pc c-btn--slide--white">
-     <a href="<<?php echo $home; ?>" class="c-btn">TOPへ</a><!-- /.c-btn -->
+     <a href="<?php echo $home; ?>" class="c-btn">TOPへ</a><!-- /.c-btn -->
     </div><!-- /.p-contact__btn -->
   </section><!-- /.l-lower-thanks -->
 
